7fd074039b34c5d2a820e967b04d3040abc50373
Juan Sanchez <juansan1993@hotmail.com>
2023-05-04 22:07:02 -0500

    Update style Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
-
-import './App.css';
+import './styles/App.scss';
 
 function App() {
   return (
