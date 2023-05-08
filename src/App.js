a6ab1a543dcffc0d5580ba864a217e1c9ada46f5
Juan Sanchez <juansan1993@hotmail.com>
2023-05-08 16:18:05 -0500

    Add redux set up with counter example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
+import Counter from './features/counter/Counter';
 import './styles/App.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
+      <Counter />
     </div>
   );
 }
