413b7e536fbcf5c8eb33e6dfe761a59d4bfdbfb2
Juan Sanchez <juansan1993@hotmail.com>
2023-05-24 14:34:12 -0500

    Delete import not used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Categories from './components/Categories';
-import './styles/App.scss';
 
 function App() {
   return (
